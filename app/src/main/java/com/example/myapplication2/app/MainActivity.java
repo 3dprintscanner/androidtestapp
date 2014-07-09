4acf3ce51fdb1f9849e9235ae06453367a8ed380
3dprintscanner <1988anthonythomas@gmail.com>
2014-07-09 20:01:31 +0100

    first commit to project

---
+++ app/src/main/java/com/example/myapplication2/app/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.myapplication2.app;
+
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends FragmentActivity {
+
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            MapsInitializer.initialize(this);
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();}}
+
+
+    private void googleMap() {
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        if (map != null) {
+            Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg"));
+            Marker kiel = map.addMarker(new MarkerOptions().position(KIEL).title("Kiel"));
+
+
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+            map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        }
+
+//    }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
